490f8f07e85c11e534ac5817d0def4e777fb7f28
cmay4 <115692966+cmay4@users.noreply.github.com>
2023-01-06 16:25:41 +0100

    python test file

---
+++ test.py
@@ -0,0 +1,25 @@
+# Dies ist ein Kommentar. Kommentare werden von Python ignoriert und dienen dazu, Code für andere Menschen lesbarer zu machen.
+
+# Das print-Statement gibt den Text in Anführungszeichen auf der Konsole aus.
+print("Hallo Welt!")
+
+# Du kannst auch Variablen definieren und ihnen Werte zuweisen.
+meine_variable = 42
+print(meine_variable)
+
+# Du kannst auch eine Berechnung in einer print-Anweisung ausführen.
+print(2 + 3)
+
+# Du kannst auch eine Bedingung in einer if-Anweisung überprüfen.
+if meine_variable > 40:
+  print("meine_variable ist größer als 40")
+
+# Du kannst auch Schleifen verwenden, um denselben Code mehrmals auszuführen.
+for i in range(5):
+  print(i)
+
+# Du kannst auch Funktionen definieren, um Code zu organisieren und wiederverwenden.
+def meine_funktion(parameter):
+  print(parameter)
+
+meine_funktion("Hallo!")
